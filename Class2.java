bc128cfe9d176ade2331e2480e7b6d3a592afa7c
Jay Snellen, III <jsnellen@jsu.edu>
2018-01-19 09:59:33 -0600

    Initial Commit

---
+++ Class2.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author Your Name
+ */
+
+public class Class2 {
+
+    private void f1() {
+        // v1.0
+    }
+    private void f2() {
+        // v2.0
+    }
+    private void f3() {
+        // v3.0
+    }
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+/**
+ *
+ * @author Your Name
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        Class c1 = new Class1();
+        Class c2 = new Class2();
+        System.out.println("Hello, World!");
+    }
+    private void f1() {
+        // v1.0
+    }
+    private void f2() {
+        // v2.0
+    }
+    private void f3() {
+        // v3.0
+    }
+
+}
